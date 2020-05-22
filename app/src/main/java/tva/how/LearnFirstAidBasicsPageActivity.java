72b6b3222808ea19502349756f3a923ce666733e
katrajh <katarina.rajh@gmail.com>
2020-05-22 17:15:56 +0200

    Dodana nova aktivnost LearnFirsfAidBasicsPageActivity

--- app/src/main/java/tva/how/LearnFirstAidBasicsPageActivity.java
+++ app/src/main/java/tva/how/LearnFirstAidBasicsPageActivity.java
@@ -1,18 +1,14 @@
 package tva.how;
 
-/**
- * UČENJAK PRVE POMOČI
- */
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LearnFirstAidBasicActivity extends AppCompatActivity {
+public class LearnFirstAidBasicsPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learn_first_aid_basic);
+        setContentView(R.layout.activity_learn_first_aid_basics_page);
     }
 }
