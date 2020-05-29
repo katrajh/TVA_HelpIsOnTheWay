cc2af11b8a1c702e4db92c72fdf5d2dd33cb5643
katrajh <katarina.rajh@gmail.com>
2020-05-29 20:16:57 +0200

    Dodan class OsnovePrvePomoci Popravljen zemljevid, da se prikaže zemljevid glede na trenutno lokacijo.

--- app/src/main/java/tva/how/DefibrilatorMapActivity.java
+++ app/src/main/java/tva/how/DefibrilatorMapActivity.java
@@ -81,10 +81,9 @@ public class DefibrilatorMapActivity extends FragmentActivity implements OnMapRe
 
             //You can still do this if you like, you might get lucky:
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
+                // TODO: Consider calling ActivityCompat#requestPermissions
                 // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                // public void onRequestPermissionsResult(int requestCode, String[] permissions,
                 //                                          int[] grantResults)
                 // to handle the case where the user grants the permission. See the documentation
                 // for ActivityCompat#requestPermissions for more details.
@@ -99,10 +98,9 @@ public class DefibrilatorMapActivity extends FragmentActivity implements OnMapRe
             } else {
                 //This is what you need:
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
+                    // TODO: Consider calling ActivityCompat#requestPermissions
                     // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    // public void onRequestPermissionsResult(int requestCode, String[] permissions,
                     //                                          int[] grantResults)
                     // to handle the case where the user grants the permission. See the documentation
                     // for ActivityCompat#requestPermissions for more details.
